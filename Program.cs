a2229cebe5c17e2ed637df8794abd92c0f3ab7fd
Luis Silva <luissilvam537@gmail.com>
2026-02-10 21:09:23 -0300

    Creating menu structure and main methods.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace FileManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("===== Gerenciamento de arquivos =====");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Editar arquivo");
+            Console.WriteLine("3 - Salir");
+
+            Console.Write("Informe a ação desejada: ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1 : OpenFile(); break;
+                case 2 : EditFile(); break;
+                case 3 : Environment.Exit(0); break;
+            }
+        }
+        
+        static void OpenFile(){}
+        static void EditFile(){}
+    }
+}
